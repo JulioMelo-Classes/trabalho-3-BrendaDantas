83d0e618dc637aa2bb08e3a979d670346dba250c
Brenda Dantas <brendasod98@gmail.com>
2021-08-19 16:21:51 -0300

    Adicionando classe Level.

---
+++ include/Level.h
@@ -0,0 +1,13 @@
+#ifndef Level_h
+#define Level_h
+
+#include <vector>
+
+class Level {
+    private:
+        int quantidade_linhas, quantidade_colunas, quantidade_comidas;
+        std::vector<std::string> mapas_do_arquivo;
+
+};
+
+#endif
