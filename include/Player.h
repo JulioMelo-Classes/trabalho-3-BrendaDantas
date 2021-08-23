53c790d43ea27648a89bbe731242a6e0a23ae91f
silviafds <75648325+silviafds@users.noreply.github.com>
2021-08-23 09:41:49 -0300

    Add files via upload

--- include/Player.h
+++ include/Player.h
@@ -1,14 +1,11 @@
 #ifndef Player_h
 #define Player_h
 
-using namespace std;
-
 class Player{
 
-    public:
-
     private:
-        //bool find_solution(string mapa_para_analisar);
+
+    public:
 
 };
 
