e975c124eb7c2165e1ac30428dbe5d58e7794652
silviafds <75648325+silviafds@users.noreply.github.com>
2021-08-23 10:05:39 -0300

    Add files via upload

--- include/Level.h
+++ include/Level.h
@@ -12,23 +12,18 @@ class Level {
         vector<int>quantidade_comidas_tot;
         //int quantidade_linhas, quantidade_colunas, quantidade_comidas;        
         std::vector<std::string> mapas_do_arquivo;
+        vector<string>mapa_auxiliar;
         vector<int>numeros;//vector c/ numeros de entrada
 
     public:
-        int getQuantidade_linhas();
-        void setQuantidade_linhas(int linhas);
-        int getQuantidade_colunas();
-        void setQuantidade_colunas(int colunas);
-        int getQuantidade_comidas();
-        void setQuantidade_comidas(int comidas);
         void preencher_numeros(int num);
         void mostrar_numeros();
         void separar_numeros();
-        //void mostra_num_separados();
         int validar_numeros();
         void preencher_mapa(string s);
-        void mostrar_mapas();
+        void mostrar_mapa_um();
         void interface_principal();
+        void encontrar_posicao_cobra(int num);
       
 
 };
--- include/Player.h
+++ include/Player.h
@@ -1,11 +1,14 @@
 #ifndef Player_h
 #define Player_h
 
+using namespace std;
+
 class Player{
 
-    private:
-
     public:
+        
+    private:
+        //bool find_solution(string mapa_para_analisar);
 
 };
 
