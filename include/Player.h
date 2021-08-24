3bbfc3fca74f8791b7fbc7af3f631cab75ddd1c5
silviafds <75648325+silviafds@users.noreply.github.com>
2021-08-24 20:48:23 -0300

    Add files via upload

--- include/Player.h
+++ include/Player.h
@@ -10,10 +10,14 @@ class Player{
     private:
         vector<int> posicao_nao_valida_lin;
         vector<int> posicao_nao_valida_col;
+
         vector<int> posicao_valida_lin;
         vector<int> posicao_valida_col;
+
+        
     public:
-        bool find_solution(int linha, int coluna, vector<int>& posicao_cobra, vector<string>& mapinha, int qtd_comida);
+        //bool find_solution(int linha, int coluna, vector<int>& posicao_cobra, vector<string>& mapinha, int qtd_comida);
+        void next_move(int linha, int coluna, vector<int>& posicao_cobra, vector<string>& mapinha);
 
 };
 
