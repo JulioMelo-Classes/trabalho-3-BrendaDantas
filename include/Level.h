0ae98cc83344eb10778cbccbc0241770ef20fe79
silviafds <75648325+silviafds@users.noreply.github.com>
2021-08-25 16:25:22 -0300

    Add files via upload

--- include/Level.h
+++ include/Level.h
@@ -7,28 +7,20 @@ using namespace std;
 
 class Level {
     private:
-        vector<int>quantidade_linhas_tot;//vector c/ as linhas
-        vector<int>quantidade_colunas_tot;//vector c/ as colunas
-        vector<int>quantidade_comidas_tot;//vector c/ as comidas
-        std::vector<std::string> mapas_do_arquivo;//vector c/ todos os mapas
-        vector<string> mapa_auxiliar;//vector q guarda os mapas por nível
-        vector<int> numeros;//vector c/ numeros de entrada
-        vector<int> posicao_cobra;//vector c/ posição da cobra
-        vector<int> posicao_da_comida_linha;//vector q guarda a posição da comida sorteda
-        vector<int> posicao_da_comida_coluna;
+        vector<string> todos_os_mapas;//recebe todos os mapas
+        vector<int> todos_os_numeros;//recebe todos os numeros
+        vector<int> so_linhas;//recebe apenas linhas
+        vector<int> so_colunas;//recebe apenas colunas
+        vector<int> so_comidas;//recebe apenas comidas
+        int comidas_somadas;//guarda a soma total de comidas que a cobra tem de comer
+
 
     public:
-        void preencher_numeros(int num);
-        void mostrar_numeros();
+        void preencher_mapa(string mapa);
+        void preencher_numeros(int numeros);
         void separar_numeros();
+        void somando_comidas();
         int validar_numeros();
-        void preencher_mapa(string s);
-        void mostrar_mapa_um();
-        void interface_principal();
-        void encontrar_posicao_cobra(int num);
-        void distribuindo_comida(int gatilho_comida);
-        void monitoramento(int num);
-        void mostrar_cobra_mapa(int num);
 
 };
 
--- include/Player.h
+++ include/Player.h
@@ -8,17 +8,7 @@ using namespace std;
 class Player{
 
     private:
-        vector<int> posicao_nao_valida_lin;
-        vector<int> posicao_nao_valida_col;
-
-        vector<int> posicao_valida_lin;
-        vector<int> posicao_valida_col;
-
-        
-    public:
-        //bool find_solution(int linha, int coluna, vector<int>& posicao_cobra, vector<string>& mapinha, int qtd_comida);
-        void next_move(int linha, int coluna, vector<int>& posicao_cobra, vector<string>& mapinha);
-        void move_snake(char head, int linha_pos, int pos_coluna, int linha_mapa_atual, int coluna_mapa_atual, vector<string>& mapa_atual, int plc, int pcc);
+    
 
 };
 
